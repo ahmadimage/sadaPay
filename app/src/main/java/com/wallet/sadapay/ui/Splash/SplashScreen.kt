932369a932675d788c77a06b7b30df19dd03a1a3
AZ <ahmadimage@gmail.com>
2023-08-20 20:10:35 +0500

    1. splash screen navigation fix

--- app/src/main/java/com/wallet/sadapay/ui/Splash/SplashScreen.kt
+++ app/src/main/java/com/wallet/sadapay/ui/Splash/SplashScreen.kt
@@ -1,20 +1,19 @@
 package com.wallet.sadapay.ui.Splash
 
-import android.window.SplashScreen
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,28 +23,40 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.wallet.sadapay.R
 import com.wallet.sadapay.navigation.Screen
 import com.wallet.sadapay.ui.theme.OrangePrimary
+import kotlinx.coroutines.delay
 
 @Composable
 fun AnimatedSplashScreen(navController: NavController) {
-    val context = LocalContext.current
-    val view = LocalView.current
-    val systemUiController = rememberSystemUiController()
 
+    val systemUiController = rememberSystemUiController()
     // Set the status bar color
     val statusBarColor = Color.Red // Replace with your desired color
     val darkIcons = statusBarColor == Color.White // Set to true if statusBarColor is light
-
     systemUiController.setStatusBarColor(
         color = OrangePrimary,
         darkIcons = darkIcons
     )
+    var startAnimation by remember { mutableStateOf(false)}
+    val alphaAnim = animateFloatAsState(
+        targetValue = if(startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
 
-    SplashScreen(navController = navController)
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(4000)
+        navController.popBackStack()
+        navController.navigate(Screen.Login.route)
+    }
+
+    SplashScreen(alphaAnim.value)
 }
 
 @Composable
 fun SplashScreen(
-    navController: NavController
+    alpha: Float
 ) {
     Box(
         modifier = Modifier
@@ -57,9 +68,7 @@ fun SplashScreen(
             modifier = Modifier
                 .size(150.dp)
                 .clip(CircleShape)
-                .clickable {
-                    navController.navigate(Screen.Login.route)
-                },
+                .alpha(alpha),
             painter = painterResource(R.drawable.ic_card),
             contentDescription = "language logo"
         )
